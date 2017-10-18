a08a6e308e913e9176580c68d0894535a7844092
Sidney Alcantara <theisidney@gmail.com>
2017-10-18 22:59:15 +1100

    need to output json

--- nesappscraper/spiders/nesapp_spider.py
+++ nesappscraper/spiders/nesapp_spider.py
@@ -1,26 +1,34 @@
 import scrapy
-from scrapy import log
+import logging
+
+from scrapy import signals
+from scrapy.xlib.pydispatch import dispatcher
 from ..items import course_item, exam_pack_item, doc_item
 
 class NesaPPSpider(scrapy.Spider):
     name = "nesapp"
-    start_urls = ['http://educationstandards.nsw.edu.au/wps/portal/nesa/11-12/Understanding-the-curriculum/resources/hsc-exam-papers']
+    #start_urls = ['http://educationstandards.nsw.edu.au/wps/portal/nesa/11-12/Understanding-the-curriculum/resources/hsc-exam-papers']
+    start_urls = ['file:///C:/Users/Sidney%20Alcantara/Desktop/test.html']
+
+    def __init__(self):
+        dispatcher.connect(self.output, signals.spider_closed)
 
     def parse(self, response):
-        # create list of course items
-        global course_items
-        course_items = []
+        # create list of course names
+        global course_names
+        course_names = []
+        # create list of exam pack lists
+        global exam_pack_items
+        exam_pack_items = []
         # loops through all courses
         for course in response.css('.res-accordion-grp'):
 
             # gets course name
             course_name = course.css('.res-heading::text').extract_first().strip()
-            # create new course_item
-            course_item_current = course_item(
-                course = course_name,
-                packs = []
-            )
-            course_items.append( course_item_current )
+            # adds to list
+            course_names.append(course_name)
+            # creates empty list in exam_pack_items
+            exam_pack_items.append([])
             # loops through all years in each course
             for year in course.css('.res-accordion-content span a'):
                 # extracts link & year for each exam pack
@@ -28,7 +36,8 @@ class NesaPPSpider(scrapy.Spider):
                 exam_pack_year = year.css('::text').re_first(r'[0-9]{4}')
 
                 # create new exam_pack_item
-                exam_pack_items = exam_pack_item(
+                exam_pack_item_current = exam_pack_item(
+                    course = course_name,
                     year = exam_pack_year,
                     link = exam_pack_link
                 )
@@ -38,8 +47,7 @@ class NesaPPSpider(scrapy.Spider):
                 # add metadata
                 exam_pack_request.meta['course'] = course_name
                 exam_pack_request.meta['year'] = exam_pack_year
-                exam_pack_request.meta['exam_pack_item'] = exam_pack_items
-                exam_pack_request.meta['course_item'] = course_item_current
+                exam_pack_request.meta['exam_pack_item'] = exam_pack_item_current
                 # add request
                 yield exam_pack_request
 
@@ -57,19 +65,14 @@ class NesaPPSpider(scrapy.Spider):
         exam_pack_item = response.meta['exam_pack_item']
         exam_pack_item['docs'] = doc_items
 
-        course_item = response.meta['course_item']
-
         # gets index of corresponding course item
-        index = course_items.index(course_item)
+        index = course_names.index(response.meta['course'])
         # append current exam pack item
-        log.msg( course_items[index], level=log.DEBUG)
-        course_items[index]['packs'].append(course_item)
-        log.msg( course_items[index]['packs'], level=log.DEBUG)
-
-        #self.group_items(exam_pack_item, course_item)
-
-#    def group_items(self, exam_pack_item, course_item):
+        exam_pack_items[index].append( exam_pack_item )
 
     def output(self):
-        for item in course_item:
-            yield item
+        for i in range(len(course_names)):
+            yield course_item(
+                course = course_names[i],
+                packs = exam_pack_items[i]
+            )
