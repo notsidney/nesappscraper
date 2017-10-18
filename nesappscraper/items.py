8c900dfccab2ead2d5d7b1b1713383c14228d67a
Sidney Alcantara <theisidney@gmail.com>
2017-10-18 21:02:53 +1100

    recursion error

--- nesappscraper/items.py
+++ nesappscraper/items.py
@@ -8,18 +8,15 @@
 import scrapy
 
 class doc_item(scrapy.Item):
-	doc_course = scrapy.Field()
-	doc_year = scrapy.Field()
 	doc_name = scrapy.Field()
 	doc_link = scrapy.Field()
 
 class exam_pack_item(scrapy.Item):
+	course = scrapy.Field()
 	year = scrapy.Field()
 	link = scrapy.Field()
 	docs = scrapy.Field()
-	pass
 
 class course_item(scrapy.Item):
     course = scrapy.Field()
     packs = scrapy.Field()
-    pass
--- nesappscraper/spiders/nesapp_spider.py
+++ nesappscraper/spiders/nesapp_spider.py
@@ -1,4 +1,5 @@
 import scrapy
+from scrapy import log
 from ..items import course_item, exam_pack_item, doc_item
 
 class NesaPPSpider(scrapy.Spider):
@@ -6,52 +7,69 @@ class NesaPPSpider(scrapy.Spider):
     start_urls = ['http://educationstandards.nsw.edu.au/wps/portal/nesa/11-12/Understanding-the-curriculum/resources/hsc-exam-papers']
 
     def parse(self, response):
+        # create list of course items
+        global course_items
+        course_items = []
         # loops through all courses
         for course in response.css('.res-accordion-grp'):
+
             # gets course name
             course_name = course.css('.res-heading::text').extract_first().strip()
-            # creates list of exam packs (per year) for each course
-            exam_packs = []
+            # create new course_item
+            course_item_current = course_item(
+                course = course_name,
+                packs = []
+            )
+            course_items.append( course_item_current )
             # loops through all years in each course
             for year in course.css('.res-accordion-content span a'):
                 # extracts link & year for each exam pack
                 exam_pack_link = response.urljoin( year.css('::attr(href)').extract_first() )
                 exam_pack_year = year.css('::text').re_first(r'[0-9]{4}')
 
+                # create new exam_pack_item
+                exam_pack_items = exam_pack_item(
+                    year = exam_pack_year,
+                    link = exam_pack_link
+                )
+
                 # scraper follows this link
                 exam_pack_request = scrapy.Request(exam_pack_link,callback=self.parse_docs)
                 # add metadata
                 exam_pack_request.meta['course'] = course_name
                 exam_pack_request.meta['year'] = exam_pack_year
+                exam_pack_request.meta['exam_pack_item'] = exam_pack_items
+                exam_pack_request.meta['course_item'] = course_item_current
                 # add request
                 yield exam_pack_request
 
-                # add new exam_pack_item to exam_packs list
-                exam_packs.append( exam_pack_item(
-                    year = exam_pack_year,
-                    link = exam_pack_link,
-                    docs = []
-                ) )
-            # create new course_item
-            global course_items
-            course_items = course_item(
-                course = course_name,
-                packs = exam_packs
-            )
-            # write json
-            yield course_items
-
     def parse_docs(self, response):
-        course_items
+        doc_items = []
         for doc in response.css('.right-menu-list a'):
-            doc_items = doc_item(
-                doc_course = response.meta['course'],
-                doc_year = response.meta['year'],
+            doc_items.append( doc_item(
+                doc_link = response.urljoin( doc.css('::attr(href)').extract_first() ),
                 doc_name = doc.css('::attr(data-file-name)').extract_first()
                     .replace(response.meta['course'],'')
                     .replace(response.meta['year'],'')
                     .strip()
-                    .capitalize(),
-                doc_link = response.urljoin( doc.css('::attr(href)').extract_first() )
-            )
-        yield doc_items
+                    .capitalize()
+            ) )
+        exam_pack_item = response.meta['exam_pack_item']
+        exam_pack_item['docs'] = doc_items
+
+        course_item = response.meta['course_item']
+
+        # gets index of corresponding course item
+        index = course_items.index(course_item)
+        # append current exam pack item
+        log.msg( course_items[index], level=log.DEBUG)
+        course_items[index]['packs'].append(course_item)
+        log.msg( course_items[index]['packs'], level=log.DEBUG)
+
+        #self.group_items(exam_pack_item, course_item)
+
+#    def group_items(self, exam_pack_item, course_item):
+
+    def output(self):
+        for item in course_item:
+            yield item
