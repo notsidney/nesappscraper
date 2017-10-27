512096841dd507898d36bdd503f79ec30a796430
Sidney Alcantara <theisidney@gmail.com>
2017-10-28 05:00:38 +1100

    add commit signing whenever batch file is run

--- nesappscraper.bat
+++ nesappscraper.bat
@@ -3,6 +3,6 @@ title NESA PP Scraper - Started %time%
 scrapy crawl nesapp
 git add data.json
 git add meta.json
-git commit -m "Update data: %date% %time%"
+git commit -S -m "Update data: %date% %time%"
 git push
 pause
