d9c5b3714197b9e28d53615f379697fcd3b9a21f
Sidney Alcantara <theisidney@gmail.com>
2017-11-01 13:09:28 +1100

    Add PowerShell script

---
+++ nesappscraper.ps1
@@ -0,0 +1,21 @@
+﻿# Create timer for window title
+$count = 0
+$timer = New-Object System.Timers.Timer
+$timer.Interval = 1000
+# Write runtime to window title
+Register-ObjectEvent -InputObject $timer -EventName Elapsed -Action {
+  $count++
+  $runtime = [timespan]::fromseconds($count).ToString("mm\:ss")
+  $host.ui.RawUI.WindowTitle = "NESA PP Scraper - Runtime $runtime"
+}
+# Run crawler
+scrapy crawl nesapp
+# Stop timer
+$timer.Stop()
+# Print meta.json
+write-host (Get-Content meta.json) -ForegroundColor Yellow
+# Push to git
+git add data.json
+git add meta.json
+git commit -S -m "Update data: %date% %time%"
+git push
