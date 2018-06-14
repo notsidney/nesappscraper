921d17b9c88556b1b58559adb8644557c571ef02
Sidney Alcantara <theisidney@gmail.com>
2018-06-15 00:23:14 +1000

    Fixed shell script

--- nesappscraper-hscpastpapers.sh
+++ nesappscraper-hscpastpapers.sh
@@ -3,19 +3,22 @@
 echo "\033]0;NESA Past Papers Scraper – Started $(date +"%T")\007"
 # Colours
 YELLOW='\033[1;33m'
+GREEN='\033[0;32m'
 NC='\033[0m'
 # Run crawler
-echo scrapy crawl nesapp
+scrapy crawl nesapp
 # Print meta.json
-echo "\n${YELLOW}$(cat meta.json)\n${NC}"
+echo "${YELLOW}$(cat meta.json)"
 # Get timestamp at end of scraping
 TIMESTAMP=$(date +"%Y-%m-%d %H:%M")
 # Push to git
+echo "\nPush to nesappscraper:"
 git add data.json
 git add meta.json
 git commit -S -m "Update data: ${TIMESTAMP}"
 git push
 # Copy to hscpastpapers git
+echo "\nPush to hscpastpapers:"
 cp data.json ../hscpastpapers/data.json
 cp meta.json ../hscpastpapers/meta.json
 # Push to hscpastpapers git
@@ -24,3 +27,4 @@ git add data.json
 git add meta.json
 git commit -S -m "Update data: ${TIMESTAMP}"
 git push
+echo "\n${GREEN}Data updated: ${TIMESTAMP}"
